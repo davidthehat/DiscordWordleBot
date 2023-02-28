042231a55a7c100a4bc686b18121791f34588104
DaTrederVid <dedemacmactretre@gmail.com>
2023-02-27 22:06:58 -0500

    improved display and security

--- wordle/index.js
+++ wordle/index.js
@@ -25,9 +25,20 @@ async function fetchSolver() {
     return data;
 }
 
+function wonGame(resultsArray) {
+    if (resultsArray === undefined) {
+        return false;
+    }
+    if (resultsArray.length != 6) {
+        return true;
+    }
+    var finalRow = resultsArray[5];
+    return finalRow.every((value) => value === "correct");
+}
+
 function generateWordleResultString(resultsArray, number, mode) {
     //loop through results array
-    var resultString = `Wordle ${number} ${resultsArray.length}/6${mode}\n\n`;
+    var resultString = `Wordle ${number} ${wonGame(resultsArray) ? resultsArray.length : "X"}/6${mode}\n\n`;
 
     for (var i = 0; i < resultsArray.length; i++) {
         for (var j = 0; j < resultsArray[i].length; j++) {
@@ -48,11 +59,18 @@ function generateWordleResultString(resultsArray, number, mode) {
     return resultString;
 }
 
-async function playWordle(mode) {
+async function playWordle(mode, word) {
     var solver = await fetchSolver();
     var wordleData = await fetchWordleData();
-    
-    solver = `simWord = "${wordleData.solution}";simMode = true;` + solver;
+    if (word === undefined) {
+        word = wordleData.solution;
+    }
+    const alpha5 = new RegExp(`^[a-z]{5}$`);
+    if (!alpha5.test(word) || word.length != 5) {
+        throw new Error("Word must be 5 letters long and only contain letters a-z");
+    }
+
+    solver = `simWord = "${word}";simMode = true;` + solver;
     console.log(solver);
     resultsArray = await eval(solver);
     return generateWordleResultString(resultsArray, wordleData.days_since_launch, mode);
