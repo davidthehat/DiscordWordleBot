7378e2a63ef0373dcd8964baffb6bfe7f71e8e21
DaTrederVid <dedemacmactretre@gmail.com>
2023-02-28 00:35:06 -0500

    Add /question command

---
+++ commands/question.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('question')
+		.setDescription('Answers your question about the wordlebot!')
+        .addStringOption(option =>
+            option.setName('question')
+                .setDescription('The question about the wordlebot you want answered')
+                .setRequired(true)),
+
+
+	async execute(interaction) {
+        //fetch wordle word
+        await interaction.reply("WordleBot is open source! You can find the source code here: https://github.com/davidthehat/DiscordWordleBot. All possible questions about the bot can be answered by reading the source code.");
+    }
+    
+};
