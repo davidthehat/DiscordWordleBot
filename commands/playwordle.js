ece4c7f1fcab2c7c67c8d61a567e146edc5eaf8c
DaTrederVid <dedemacmactretre@gmail.com>
2023-02-27 22:06:45 -0500

    Added options to set mode and word

--- commands/playwordle.js
+++ commands/playwordle.js
@@ -4,9 +4,34 @@ const wordle = require('../wordle');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('playwordle')
-		.setDescription("Plays todays wordle and replies with the bot's results!"),
+		.setDescription("Plays todays wordle and replies with the bot's results!")
+        .addStringOption(option =>
+            option.setName('mode')
+                .setDescription('The mode to play the wordle in')
+                .setRequired(false))
+        .addStringOption(option =>
+            option.setName('word')
+                .setDescription('The word to play the wordle with')
+                .setRequired(false)),
+
 	async execute(interaction) {
-        x = await wordle.playWordle(" :robot:");
+        if (interaction.options.getString('mode') == null) {
+            var mode = " :robot:";
+        } else {
+            var mode = interaction.options.getString('mode');
+        }
+        if (interaction.options.getString('word') == null) {
+            var word = undefined;
+        } else {
+            var word = interaction.options.getString('word');
+        }
+        try {
+            var x = await wordle.playWordle(mode, word);
+        }
+        catch (error) {
+            await interaction.reply("Error: " + error.message);
+            return;
+        }
         await interaction.reply(x);
 	},
 };
