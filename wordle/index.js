4e8e3cf4965a11b72d56b027e95ced1e2c50a878
DaTrederVid <dedemacmactretre@gmail.com>
2023-02-28 00:35:22 -0500

    Fix bug where single digit dates did not work.

--- wordle/index.js
+++ wordle/index.js
@@ -5,7 +5,7 @@ function dateToWordledate(date) {
     if (date === undefined) {
         date = new Date();
     }
-    return date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + date.getDate();
+    return date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + (date.getDate()).toString().padStart(2, "0");
 }
 
 async function fetchWordleData(date) {
