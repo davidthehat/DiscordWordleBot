7a637032fddc84b5dc8530d14da37e5fbc1798fe
DaTrederVid <dedemacmactretre@gmail.com>
2023-03-01 16:08:15 -0500

    Update wordleGPT.js

--- commands/wordleGPT.js
+++ commands/wordleGPT.js
@@ -1,9 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+//openai setup
+const { Configuration, OpenAIApi } = require("openai");
+const { OPENAI_API_KEY, PROMPT_HEADER } = require('../openai/config.json')
+
+const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('wordleGPT')
+		.setName('wordlegpt')
 		.setDescription('Answers your question AS the wordlebot!')
         .addStringOption(option =>
             option.setName('question')
@@ -12,7 +22,18 @@ module.exports = {
 
 
 	async execute(interaction) {
-        
+        try {
+            await interaction.deferReply();
+            const completion = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: PROMPT_HEADER + "\n\n" + interaction.options.getString('question'),
+              max_tokens: 100
+            });
+            console.log(completion.data.choices)
+            await interaction.editReply(completion.data.choices[0].text);
+        } catch (error) {
+            await interaction.editReply(`There was an error while executing this command: ${error}`);
+        }
     }
     
 };
