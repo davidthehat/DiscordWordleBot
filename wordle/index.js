d1ab69a44bb6dead9166efe5fcefb92f23105d19
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-03-07 17:21:34 -0500

    Added chairMode function, made it so when :chair: is used as mode, chairMode is used instead of wordlebot

--- wordle/index.js
+++ wordle/index.js
@@ -83,6 +83,44 @@ function generateWordleResultString(resultsArray, number, mode, revealed) {
     return resultString;
 }
 
+function chairMode(answerWord) {
+  var gradeGuess = function(guess) {
+    var arr = Array(guess.length).fill("absent");
+    simWordCopy = answerWord.split("");
+
+    for (var i = 0; i < guess.length; i++) {
+        if (simWordCopy[i] == guess[i]) {
+            arr[i] = 'correct';
+            simWordCopy[i] = "_";
+        }
+    }
+
+    for (var i = 0; i < guess.length; i++) {
+        if (simWordCopy.includes(guess[i]) && arr[i] != 'correct') {
+            arr[i] = 'present';
+            for (var j = 0; j < simWordCopy.length; j++) {
+                if (simWordCopy[j] == guess[i]) {
+                    simWordCopy[j] = "_";
+                    break;
+                }
+            }
+        }
+    }
+    return arr;
+  }
+
+  var chair = 'CHAIR'
+  var gradeArr = gradeGuess(chair)
+  var resultsArr = []
+  resultsArr.concat([[gradeArr, chair]])
+  resultsArr.concat([[gradeArr, chair]])
+  resultsArr.concat([[gradeArr, chair]])
+  resultsArr.concat([[gradeArr, chair]])
+  resultsArr.concat([[gradeArr, chair]])
+  resultsArr.concat([[gradeArr, chair]])
+  return resultsArr
+}
+
 async function playWordle(mode, wordleData, word, revealed) {
     var solver = await fetchSolver();
     
@@ -96,6 +134,13 @@ async function playWordle(mode, wordleData, word, revealed) {
     if (!alpha5.test(word) || word.length != 5) {
         throw new Error("Word must be 5 letters long and only contain letters a-z");
     }
+
+    if (mode == ':chair:'){
+      resultsArray = chairMode(word);
+      console.log("resultsArray: ")
+      console.log(resultsArray);
+      return generateWordleResultString(resultsArray, number, mode, revealed);
+    } 
 
     solver = `verbose = false; simWord = "${word}";simMode = true;` + solver;
     resultsArray = await eval(solver);
