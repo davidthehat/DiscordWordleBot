03410e3b34b18a5ed8e26c5859d3fd065fd69137
DaTrederVid <dedemacmactretre@gmail.com>
2023-06-17 15:50:21 -0400

    Queue on options panel

--- commands/music.js
+++ commands/music.js
@@ -25,20 +25,40 @@ module.exports = {
 		.setDescription('Plays music! (Very WIP. Expect bugs and missing features.)')
         .addStringOption(option =>
             option.setName('name')
-                .setDescription('The name of the song to play')
+                .setDescription('The name of the song to play. Omit to open options panel.')
                 .setRequired(false)),
 
 
     async execute(interaction) {
         //check if name is provided
         if (interaction.options.getString('name') == null) {
+            //get queue for this guild
+            const guild = interaction.guild;
+            const queue = player.nodes.get(guild);
+            const tracks = queue.tracks.data;
+
+            //get first 20 tracks
+            const tracks20 = tracks.slice(0,20);
+
             //send GUI embed
+
             const embed = new EmbedBuilder()
                 .setColor('#0099ff')
                 .setTitle('Options Panel')
                 .setDescription('WIP Music Player')
-                .setTimestamp();
-            
+                .setTimestamp()
+                //for each track, add a field
+                .addFields(
+                    tracks20.map((track, index) => {
+                        console.log(track);
+                        return {
+                            name: `${index + 1}. ${track.title}`,
+                            value: `Duration: ${track.duration}\nRequested by: ${track.requestedBy}`,
+                            inline: false
+                        }
+                    })
+                );
+                            
             //create action row with link button
             const actionRow = new ActionRowBuilder()
                 .addComponents(BackButton.data)
@@ -56,6 +76,7 @@ module.exports = {
         }
 
         const channel = interaction.member.voice.channel; // get the voice channel of the user who sent the interaction
+        const user = interaction.user; // get the user who sent the interaction
         if (!channel) return interaction.reply('You are not connected to a voice channel!'); // make sure we have a voice channel
         const query = interaction.options.getString('name', true); // we need input/query to play
     
@@ -63,6 +84,7 @@ module.exports = {
         await interaction.deferReply();
         const data = await yts.GetListByKeyword(query,false,1,[{type:'video'}]);
         let vidId = data['items'][0]['id'];
+                
         try {
             const { track } = await player.play(channel, vidId, {
                 nodeOptions: {
@@ -70,8 +92,14 @@ module.exports = {
                     metadata: interaction // we can access this metadata object using queue.metadata later on
                 }
             });
+            track.requestedBy = user.tag; // let's add who requested this track
+            //remove #0 if it ends user tag
+            if (track.requestedBy.endsWith('#0')) {
+                track.requestedBy = track.requestedBy.slice(0, -2);
+            }
+            
     
-            return interaction.followUp(`**${track.title}** enqueued!`);
+            return interaction.followUp(`**${track.title}** wordled!`);
         } catch (e) {
             // let's return error if something failed
             return interaction.followUp(`Something went wrong: ${e}`);
